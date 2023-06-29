4f2fbcb95ac2660cbde5bd0227e596e38a3174d9
LordZeuz <chriskwesi12@gmail.com>
2023-06-29 00:21:39 +0000

    pascals triangle1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
+"""
+  Generate Pascal's triangle up to the nth row.
+
+  Args:
+      n (int): The number of rows for Pascal's triangle.
+
+  Returns:
+     list: List of lists representing Pascal's triangle.
+"""
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
